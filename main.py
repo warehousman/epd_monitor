82f3f008b9c3a8279a186c17f9e73b8ac8d60b5b
warehousman <warehousman@gmail.com>
2018-10-15 19:58:46 +0300

    fix

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ def show():
     # write strings to the buffer
     epd.display_string_at(frame_black, 20, 60, data['gpu-temperature'], font_b, COLORED)
     epd.display_string_at(frame_black, 110, 60, data['gpu-usage'], font_b, COLORED)
-    epd.display_string_at(frame_black, 20, 60, data['cpu-temperature'], font_b, COLORED)
-    epd.display_string_at(frame_black, 110, 60, data['cpu-usage'], font_b, COLORED)
+    epd.display_string_at(frame_black, 20, 160, data['cpu-temperature'], font_b, COLORED)
+    epd.display_string_at(frame_black, 110, 160, data['cpu-usage'], font_b, COLORED)
     # display the frame
     epd.display_frame(frame_black, frame_red)
 
